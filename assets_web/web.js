2e83dab993c31ea35f28aeebfe8e42f977c9905d
sofiapetraglio <sofia.petraglio@supsi.ch>
2024-01-16 16:10:25 +0100

    done calibration on the circumference

--- assets_web/web.js
+++ assets_web/web.js
@@ -3,12 +3,27 @@ const layersContainer = document.getElementById("layers_container");
 
 // scroll datas
 let currentLine = 0;
+let currentIndex = 0;
+let circonferenza = 910.6; // in mm
 
 function startConstruction() {
     function dataLoop() {
         setTimeout(() => {
+            // Check if the current line is finished
+            if (currentIndex >= dataList[currentLine].length - 1) {
+                // Move to the next line
+                currentLine++;
+                currentIndex = 0; // Reset the current index for the new line
+            }
+
             // Compose the gcode command for X
-            let gcodeX = "G1 X-" + (dataList[currentLine][0]*10) + " Z-10 F5000";
+
+            let current_z = (dataList[currentLine][currentIndex]/100 * circonferenza);
+
+            console.log("current_z: " + current_z);
+            
+            let gcodeX = "G1 X" + String(current_z) + " Z" + String(current_z - 5.0) + " F1000";
+
             console.log(gcodeX);
 
             // Send the gcode to the server
@@ -19,10 +34,8 @@ function startConstruction() {
             layerContainer.classList.add("layer_container");
             layersContainer.prepend(layerContainer);
 
-            /*
-            // Increment the current data list line
-            currentLine++;      
-            */
+            // Increment the current data list index for Z
+            currentIndex++;
 
             // Check if the list is finished
             if (currentLine > dataList.length - 1) {
@@ -31,30 +44,28 @@ function startConstruction() {
                 // Continue with Y-axis movement after a timeout
                 setTimeout(() => {
                     // Compose the gcode command for Y
-                    let gocodeZ = "G1 Z0";
-                    let gcodeY = "G1 Y10 F2000";
+                    let gcodeY = "G1 Y25 Z5 F1000";
                     console.log(gcodeY);
 
                     // Send the gcode to the server
                     sendGcode(gcodeY);
 
-                    // Increment the current data list line
-                    currentLine++;
-
-                    // Check if the list is finished
-                    if (currentLine > dataList.length - 1) {
-                        currentLine = 0; // Reset the current line
-                    } else {
-                        // Continue the data loop
-                        dataLoop();
-                    }
+                    // Continue the data loop
+                    dataLoop();
                 }, 1000); // Timeout for Y-axis movement
             }
-        }, 1000); // Timeout for X-axis movement
+        //}, dataList[currentLine][dataList[currentLine].length-1]*1000); // Timeout for X-axis movement
+         }, 10000); // Timeout for X-axis movement
     }
 
+    // Initialize variables
+    let currentLine = 0;
+    let currentIndex = 0;
+
+    // Start the data loop
     dataLoop();
 }
+
 
 
 function stopConstruction() {
