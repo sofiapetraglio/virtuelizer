6c1893d5bb99288cb662e354bbd820e7e8685300
sofiapetraglio <sofia.petraglio@supsi.ch>
2024-01-16 22:27:24 +0100

    repaired without delay

--- assets_web/web.js
+++ assets_web/web.js
@@ -8,47 +8,43 @@ let circonferenza = 910.6; // in mm
 let circonferenza_duration = 0; // durata in ms di un giro di circonferenza
 
 function startConstruction() {
+
+    dataLoop();
     function dataLoop() {
 
-            setTimeout(() => {
-
                 lineLoop();
-
                 function lineLoop() {
-                    // Check if the current line is finished
-                    if (currentIndex >= dataList[currentLine].length - 1) {
-                        // Move to the next line
-                        currentLine++;
-                        currentIndex = 0; // Reset the current index for the new line
-                    }
+                    // Check if all lines are finished
+                    if (currentLine >= dataList.length) {
+                        currentLine = 0; // Reset to the first line
+                    } else {
+                        // Check if the current line is finished
+                        if (currentIndex >= dataList[currentLine].length - 1) {
+                            currentLine++; // Move to the next line
+                            currentIndex = 0; // Reset the current index for the new line
+                        }
 
-                    if (dataList[currentLine][currentIndex] != 0) {
+                        // Check if the parameter is different than 0 and has to be printed    
+                        if (dataList[currentLine][currentIndex] != 0) {
 
-                        // Compose the gcode command for X
+                            // Compose the gcode command for X
+                            let current_z = (dataList[currentLine][currentIndex]/100 * circonferenza);
+                            console.log("current_z: " + current_z);
+                            
+                            let gcodeX = "G1 X" + String(current_z) + " Z" + String(current_z - 5.0) + " F1000";
+                            console.log(gcodeX);
 
-                        let current_z = (dataList[currentLine][currentIndex]/100 * circonferenza);
+                            // Send the gcode to the server
+                            sendGcode(gcodeX);
 
-                        console.log("current_z: " + current_z);
-                        
-                        let gcodeX = "G1 X" + String(current_z) + " Z" + String(current_z - 5.0) + " F1000";
+                            // Add new layer on the HTML page inside the layerContainer
+                            const layerContainer = document.createElement("div");
+                            layerContainer.classList.add("layer_container");
+                            layersContainer.prepend(layerContainer);
 
-                        console.log(gcodeX);
+                            // Increment the current data list index for Z
+                            currentIndex++;
 
-                        // Send the gcode to the server
-                        sendGcode(gcodeX);
-
-                        // Add new layer on the HTML page inside the layerContainer
-                        const layerContainer = document.createElement("div");
-                        layerContainer.classList.add("layer_container");
-                        layersContainer.prepend(layerContainer);
-
-                        // Increment the current data list index for Z
-                        currentIndex++;
-
-                        // Check if the list is finished
-                        if (currentLine > dataList.length - 1) {
-                            currentLine = 0; // Reset the current line
-                        } else {
                             // Continue with Y-axis movement after a timeout
                             // Compose the gcode command for Y
                             let gcodeY = "G1 Y25 Z5 F1000";
@@ -57,24 +53,28 @@ function startConstruction() {
                             // Send the gcode to the server
                             sendGcode(gcodeY);
 
-                            // Continue the data loop
                             lineLoop();
-                        }
+
+                        // If parameter is 0 move to the next index
+                        } else {
+                            currentIndex++;
+                            lineLoop();
                     }
+                    }
+            }
+
+                //console.log("delay: " + dataList[currentLine][dataList[currentLine].length-1]);
+            //}, dataList[currentLine][dataList[currentLine].length-1]*1000)+ circonferenza_duration; // Timeout between one line and the other
                 }
-
-                console.log("delay: " + dataList[currentLine][dataList[currentLine].length-1]);
-                dataLoop();
-            }, dataList[currentLine][dataList[currentLine].length-1]*1000)+ circonferenza_duration; // Timeout between one line and the other
-    }
-
+/*
     // Initialize variables
     let currentLine = 0;
     let currentIndex = 0;
-
+*/
     // Start the data loop
     dataLoop();
-}
+    }
+
 
 
 
