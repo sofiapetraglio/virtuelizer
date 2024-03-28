6bdcc5b7870a154839a550cb42f54b95eb0b9392
sofiapetraglio <sofia.petraglio@supsi.ch>
2024-03-28 17:26:42 +0100

    Update read_and_array.ino

--- virtuelizer LAUDS/arduino/read_and_array/read_and_array.ino
+++ virtuelizer LAUDS/arduino/read_and_array/read_and_array.ino
@@ -52,5 +52,5 @@ void loop() {
   
   Serial.println(values);  // Print the string containing the values
 
-  delay(120000);  // Delay for 5 minutes (300,000 milliseconds)
+  delay(30000);  // Delay for 5 minutes (300,000 milliseconds)
 }
